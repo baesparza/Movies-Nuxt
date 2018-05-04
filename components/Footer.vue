79cf870a95fc2c13c2f4b755d86f3df31be53cc4
Bruno Esparza <bruno.be81@gmail.com>
2018-05-04 14:18:11 -0500

    feat(layout):  add default

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <footer>
+          With Love by <a href="https://github.com/baesparza" target="_blank">baesparza</a>
+      </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 20px;
+}
+footer > a {
+  text-decoration: none;
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+   <nav>
+        <ul class="container">
+            <li>
+              <router-link :to="{ name: 'index'}">
+                Home
+              </router-link>
+            </li>
+            <li class="search">
+              <input class="search-input" @keyup.enter="$router.push('/search/' + $event.target.value)" placeholder="Search movie">
+            </li>
+            <li>
+              <router-link :to="{ name: 'about'}">
+                About
+              </router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+
+<style scoped>
+nav {
+  list-style: none;
+}
+nav > ul > li {
+  display: inline;
+}
+nav > ul > li > a {
+  text-decoration: none;
+  margin-top: 5px;
+  display: block;
+  text-align: center;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.search-input {
+  width: 100%;
+  box-sizing: border-box;
+  background-color: transparent;
+  border: none;
+}
+.router-link-exact-active {
+  font-weight: 700;
+}
+.container {
+  display: flex;
+  text-align: center;
+}
+.search {
+  flex: 1;
+}
+.container > li {
+  flex: 1;
+}
+@media all and (max-width: 600px) {
+  .container {
+    flex-wrap: wrap;
+  }
+  .container > li {
+    flex-basis: 50%;
+  }
+}
+@media all and (max-width: 400px) {
+  .container > li {
+    flex-basis: 100%;
+  }
+  .search {
+    order: 1;
+  }
+}
+</style>

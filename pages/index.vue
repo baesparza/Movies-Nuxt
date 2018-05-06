b09419bde03d2c6617786c1bcc27bd95fcb08e9a
Bruno Esparza <bruno.be81@gmail.com>
2018-05-05 22:29:39 -0500

    feat(route): add kids movies page

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,16 @@
 <template>
-    <div class="container-fluid">
-        <div class="card-container">
-            <div v-for="(movie, index) in movies" :key='index'>
-                <router-link tag="div" :to="{ name: 'movie-id', params: { id: movie.id }}" :title="movie.title" class="item">
-                    <img :src="'http://image.tmdb.org/t/p/w500' + movie.backdrop_path" class="img-responsive">
-                    <h3>
-                        {{ movie.original_title }}
-                    </h3>
-                    <span>{{ movie.release_date }}</span>
-                </router-link>
-            </div>
-        </div>
-    </div>
+  <div class="container-fluid">
+    <ul>
+      <li><router-link :to="{ name: 'index'}">Popular</router-link></li>
+      <li><router-link :to="{ name: 'index-kids'}">Kids</router-link></li>
+	  </ul>
+    <router-view></router-view>
+  </div>
 </template>
 
 
 <script>
-import axios from "axios";
 export default {
-  asyncData() {
-    return axios
-      .get(
-        "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1c644f63e63f2e9433d17372c9322ca1"
-      )
-      .then(res => {
-        return { movies: res.data.results };
-      });
-  },
   head () {
     return {
       title: "Movies-vue",
@@ -39,7 +23,7 @@ export default {
 </script>
 
 
-<style scoped>
+<style>
 .card-container {
   display: grid;
   grid-template-columns: repeat(auto-fill, 320px);
